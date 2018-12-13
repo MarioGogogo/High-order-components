cc927de46fffff1ea1ce3daa6b5c882c25f15967
lovewcc <148373644@qq.com>
2018-12-14 00:06:11 +0800

    修改引入

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from './App';
-import App from './ref';
+import App from './App';
+// import App from './ref';
 
 ReactDOM.render(<App />, document.getElementById('root'));
